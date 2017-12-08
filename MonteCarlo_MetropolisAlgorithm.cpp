59b7dc5b65bc5eeb12d55575a8c8945557a1bf71
Arash from Win 10 <arash.gol-mohammadi@epfl.ch>
2017-12-08 20:13:53 +0100

    Metropolis Revised

--- MonteCarlo_MetropolisAlgorithm.cpp
+++ MonteCarlo_MetropolisAlgorithm.cpp
@@ -2,7 +2,12 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
+#include <random>
+#include <chrono>
 
+
+using namespace std;
 
 // Constructor and destructor
 
@@ -12,37 +17,109 @@ MonteCarlo_MetropolisAlgorithm::~MonteCarlo_MetropolisAlgorithm(){};
 // Integrator
 
 double MonteCarlo_MetropolisAlgorithm::Integrator() {
-    srand(time(NULL));
+    //srand(time(NULL));
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator (seed);
+    std::uniform_real_distribution<double> distribution(0.0,1.0);
+
     double a = GetLowerLimit();
     double b = GetUpperLimit();
     int N = GetSamplingNumber();
     int m = GetMoment();
 
-
+    int r=0;
     double sum = 0;
     double p_old, p_new,x_old,x_new;
-    double delta=0.1;
+    double delta = (b-a)*.5;
 
-    x_new=(a+b)/2;
-    p_new = FunctionValue(x_new)*pow(x_new,m);
+    int l=10;
+    N= N*l;
 
-    for (int i = 1; i <= N; ++i){
+    x_new = a+(b-a)*distribution(generator);//((double)rand()/RAND_MAX);
+    cout<<x_new;
+    p_new = FunctionValue(x_new)*pow(x_new,m)/(x_new+1)*1.5;
 
-        x_old=x_new;
+/*
+    int N_c=N/100;
+    double *x = new double[N_c];
+    for (int i = 0; i <= N_c; ++i) {
+
+        x_old = x_new;
         p_old = p_new;
 
-        x_new = x_old + 2*(delta)*((double)rand()/RAND_MAX - 0.5);
-        p_new = FunctionValue(x_new)*pow(x_new,m);
-
-        if ((double)rand()/RAND_MAX < p_new/p_old){     //rejection happens
-            x_new=x_old;
-            sum = sum + p_old;
+        x_new = x_old + 2 * (delta) * (distribution(generator) - 0.5);
+        if (x_new > b) { x_new = b - (x_new - b); }
+        if (x_new < a) { x_new = a + (a - x_new); }
+        p_new = FunctionValue(x_new) * pow(x_new, m);
+        if (distribution(generator) > fabs(p_new / p_old)) {     //rejection happens
+            //حواستمون باشه که p_old ممکنه صفر بشه. باید یه cerr بذاریم
+            x_new = x_old;
+            p_new = p_old;
         }
-        else{                                           //acceptance
-            sum = sum + p_new;
-        }
-
+        x[i]=x_new;
     }
 
-    return sum/N*(b-a);
+    double ratio=1;
+    for (int j = 0; j <= N_c/10; ++j){
+        double x_bar=0;
+        double x2_bar=0;
+        double xx=0;
+        for (int i = 0; i <= N_c/10; ++i) {
+            x_bar += x[i];
+            x2_bar += x[i] * x[i];
+        }
+    }
+
+    for (int l=0; l<N_c/10;l++){
+        for (int j = 0; j <= N_c/10; ++j){
+            double x_bar=0;
+            double x2_bar=0;
+            double xx=0;
+            for (int i = 0; i <= N_c/10; ++i) {
+                x_bar += x[i];
+                x2_bar += x[i]*x[i];
+                xx += x[i];
+            }
+
+        }
+    }
+
+
+
+
+*/
+
+
+    for (int i = 0; i < N; ++i){
+
+        x_old = x_new;
+        p_old = p_new;
+
+        x_new = x_old + 2*(delta)*(distribution(generator) - 0.5);
+        if (x_new>b){ x_new = b -(x_new-b);}
+        if (x_new<a){ x_new = a +(a-x_new);}
+        p_new = FunctionValue(x_new)*pow(x_new,m)/(x_new+1)*1.5;
+
+        if (distribution(generator) > fabs((1+x_new)/(1+x_old))) {     //rejection happens
+            //حواستمون باشه که p_old ممکنه صفر بشه. باید یه cerr بذاریم
+            x_new = x_old;
+            p_new = p_old;
+            r++;
+        }
+
+        if (i%l==0){
+            sum = sum +p_new;
+        }
+        /*
+        if (i%100==0){
+            if ((double) r/N*100)<0.3){
+                delta*=2;
+            }
+            else()
+        }
+         */
+    }
+
+    cout<<"\n"<<(double) r/N*100<<endl;
+    return sum/N*(b-a)*l;
 }
--- main.cpp
+++ main.cpp
@@ -7,26 +7,31 @@
 
 double f(double x)
 {
-    return x;
+    return tan(x);
 }
 using namespace std;
 
 int main() {
+
     MonteCarlo_UniformSampling I;
     I.SetLowerLimit(0);
     I.SetUpperLimit(1);
-    I.SetSamplingNumber(1000000);
+    I.SetSamplingNumber(10000);
     I.SetFunction(f);
     //I.SetMoment(1);
-    std::cout << "Result of SS: " <<I.Integrator()<< std::endl;
+
+
 
     MonteCarlo_MetropolisAlgorithm J;
-    J.SetLowerLimit(0);
+    J.SetLowerLimit(-0);
     J.SetUpperLimit(1);
-    J.SetSamplingNumber(1000000000);
+    J.SetSamplingNumber(10000);
     J.SetFunction(f);
     //J.SetMoment(1);
-    std::cout << "Result o MC: " <<J.Integrator()<< std::endl;
-
+    double Jans=J.Integrator();
+    double Ians=I.Integrator();
+    std::cout << "Result of SS: " <<Ians<< std::endl;
+    std::cout << "Result o MC: " <<Jans<< std::endl;
+    std::cout << "J-I: " <<Jans-Ians<< std::endl;
     return 0;
 }
